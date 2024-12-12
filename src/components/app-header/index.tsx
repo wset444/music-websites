781f087194c3ff35600125a1f6e60f8d79985b4e
russlwest44 <11502444+suqi-zer@user.noreply.gitee.com>
2024-12-13 01:14:35 +0800

    封装css公共样式 完善header头部左侧样式

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,18 +1,46 @@
 import React, { ReactNode, memo } from 'react';
 import type, { FC } from 'react';
 import { Link } from 'react-router-dom'; // 只需要导入 useRoutes
+import { AppHeaderWrapper, HeaderLeft, HeaderRight } from './style';
+
+import { headerLinks } from '@/assets/data/local-data';
+
 interface IProps {
   children?: ReactNode;
 }
 
 const Haeder: FC<IProps> = (props: IProps) => {
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return <Link to={item.link}>{item.title}</Link>;
+    } else if (item.type === 'link') {
+      return (
+        <a href={item.link} rel="noopener noreferrer" target="_blank">
+          {item.title}
+        </a>
+      );
+    }
+  }
   return (
-    <div className="nav">
-      <Link to="/discover">发现音乐</Link>
-      <Link to="/mine">我的音乐</Link>
-      <Link to="/focus">关注</Link>
-      <Link to="/download">下载客户端</Link>
-    </div>
+    <AppHeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="">
+            网易云音乐
+          </a>
+          <div className="select-list">
+            {headerLinks.map((item) => {
+              return (
+                <div className="select-item" key={item.link}>
+                  {showItem(item)}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>111111111111</HeaderRight>
+      </div>
+    </AppHeaderWrapper>
   );
 };
 export default memo(Haeder);
